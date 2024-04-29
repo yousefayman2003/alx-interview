d81240343194ce88531f65ed9a138684f7766e5e
Yousef Ayman <yousefayman20003@gmail.com>
2024-04-29 07:41:13 -0700

    Problem solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module containing Lockboxes problem solution"""
+
+
+def canUnlockAll(boxes):
+    """
+        determines if all the boxes can be opened
+
+        Args:
+            boxes (list): a list of lists
+
+        Returns:
+            True if all boxes can be opened, else return False
+    """
+    visited = set()
+
+    def dfs(i):
+        """
+            Perform depth first search on boxes keys
+
+            i (int): index of box to open
+        """
+        if i in visited:
+            return
+
+        visited.add(i)
+        for n in boxes[i]:
+            dfs(n)
+
+    # Start DFS
+    dfs(0)
+
+    return len(visited) == len(boxes)
