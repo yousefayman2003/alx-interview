9f2078716fd9b99490dda02a1bcbe8ffb9c26454
Yousef Ayman <yousefayman20003@gmail.com>
2024-04-30 05:13:45 -0700

    handled edge case

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,6 +12,9 @@ def canUnlockAll(boxes):
         Returns:
             True if all boxes can be opened, else return False
     """
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+
     visited = set()
 
     def dfs(i):
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -10,3 +10,9 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[]]
+print(canUnlockAll(boxes))
+
+boxes = []
+print(canUnlockAll(boxes))
